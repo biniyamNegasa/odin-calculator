fd96aaa204dc27ad6f2320684c356f5351770d60
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-24 16:47:01 +0300

    Implement operate function

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 function add(a, b){
-
+    return (a+b);
 };
 
 function substract(a, b){
-
+    return (a-b);
 };
 
 function multiply(a, b){
-
+    return (a*b);
 };
 
 function divide(a, b){
+    return (a/b);
+}
 
+function operate(firstNumber, operator, secondNumber){
+    switch (operator){
+        case "+":
+            add(firstNumber, secondNumber);
+            break;
+        
+        case "-":
+            substract(firstNumber, secondNumber);
+            break;
+        
+        case "*":
+            multiply(firstNumber, secondNumber);
+            break;
+        
+        case "/":
+            divide(firstNumber, secondNumber);
+            break;
+    }
 }
 
 let firstNumber;
