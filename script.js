6913940a739fb6f1ae99a3ac4bfd7fb09ab3f6d6
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-25 07:53:25 +0300

    Make operators work

--- script.js
+++ script.js
@@ -15,26 +15,96 @@ function divide(a, b){
 }
 
 function operate(firstNumber, operator, secondNumber){
+    firstNumber = +firstNumber;
+    secondNumber = +secondNumber;
+    let result = 0;
     switch (operator){
         case "+":
-            add(firstNumber, secondNumber);
+            result = add(firstNumber, secondNumber);
             break;
         
         case "-":
-            substract(firstNumber, secondNumber);
+            result = substract(firstNumber, secondNumber);
             break;
         
-        case "*":
-            multiply(firstNumber, secondNumber);
+        case "×":
+            result = multiply(firstNumber, secondNumber);
             break;
         
-        case "/":
-            divide(firstNumber, secondNumber);
+        case "÷":
+            result = divide(firstNumber, secondNumber);
             break;
     }
+    return result;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+let displayNumber = '';
 
+function populate(num){
+    displayNumber += num;
+}
+
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+const numbers = [];
+numbers.push(document.querySelector("button.zero"));
+numbers.push(document.querySelector("button.one"));
+numbers.push(document.querySelector("button.two"));
+numbers.push(document.querySelector("button.three"));
+numbers.push(document.querySelector("button.four"));
+numbers.push(document.querySelector("button.five"));
+numbers.push(document.querySelector("button.six"));
+numbers.push(document.querySelector("button.seven"));
+numbers.push(document.querySelector("button.eight"));
+numbers.push(document.querySelector("button.nine"));
+
+
+const operations = [];
+operations.push(document.querySelector("button.plus"));
+operations.push(document.querySelector("button.minus"));
+operations.push(document.querySelector("button.times"));
+operations.push(document.querySelector("button.divide"));
+
+const equalToButton = document.querySelector("button.equal-to");
+const clearButton = document.querySelector("button.clear");
+
+const display = document.querySelector(".display");
+
+for (let number of numbers){
+    number.addEventListener("click", () => {
+        populate(number.textContent);
+        display.textContent = displayNumber;
+    });
+}
+
+for (let operation of operations){
+    operation.addEventListener("click", () => {
+        if (!firstNumber){ 
+            firstNumber = displayNumber;
+            displayNumber = '';
+        }
+        else if (!secondNumber){
+            secondNumber = displayNumber;
+            displayNumber = operate(firstNumber, operator, secondNumber);
+            display.textContent = displayNumber;
+            firstNumber = displayNumber;
+            secondNumber = '';
+            displayNumber = '';
+        }
+        operator = operation.textContent;
+    });
+}
+
+equalToButton.addEventListener("click", () => {
+    if (firstNumber){
+        secondNumber = displayNumber;
+        displayNumber = operate(firstNumber, operator, secondNumber);
+        display.textContent = displayNumber;
+        firstNumber = displayNumber;
+        secondNumber = '';
+        displayNumber = '';
+    }
+});
